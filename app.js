ded116248f9e39bfb60c6015f5b18d542808bbb4
Makktu <johnmcna@gmail.com>
2022-05-09 18:36:31 +0100

    addons

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
-const btn1 = document.getElementById("input-reason");
+const reasonInput = document.querySelector("#input-reason");
+const amountInput = document.querySelector("#input-amount");
+const cancelBtn = document.querySelector("#btn-cancel");
+const confirmBtn = document.querySelector("#btn-confirm");
 
-btn1.addEventListener("click", () => {
-    console.log("clicked!");
+const expensesList = document.querySelector("#expenses-list");
+
+const clearFields = function () {
+    reasonInput.value = "";
+    amountInput.value = "";
+};
+
+confirmBtn.addEventListener("click", () => {
+    if (amountInput.value && reasonInput.value) {
+        const enteredReason = reasonInput.value;
+        const enteredAmount = amountInput.value;
+        const newItem = document.createElement("ion-item");
+        newItem.textContent += enteredReason + ": £" + enteredAmount;
+        expensesList.appendChild(newItem);
+        clearFields();
+    } else {
+        console.log("enter a value");
+    }
+});
+
+cancelBtn.addEventListener("click", () => {
+    if (amountInput.value || reasonInput.value) {
+        clearFields();
+    }
 });
